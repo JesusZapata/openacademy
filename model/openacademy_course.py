d77b057d5336e22fa8d5f406deba346a2cc4c2c3
JesusZapata <jesus@vauxoo.com>
2016-12-13 11:28:18 -0400

    [REF] openacademy: Corrigiendo el estandar pep8

--- model/openacademy_course.py
+++ model/openacademy_course.py
@@ -4,10 +4,11 @@ from openerp import fields, models
 This module create model of Course
 '''
 
+
 class Course(models.Model):
-    '''
+    """"
     This class create model of Course
-    '''
+    """
     _name = 'openacademy.course'
 
     name = fields.Char(string='Title', required=True)
