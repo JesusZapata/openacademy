50a8c4a2244b51bc22035340e6faae794f0b485d
JesusZapata <jesus@vauxoo.com>
2016-12-13 11:00:38 -0400

    [IMP] openacademy: Documenting the class Course

--- model/openacademy_course.py
+++ model/openacademy_course.py
@@ -1,7 +1,13 @@
 from openerp import fields, models
 
+'''
+This module create model of Course
+'''
 
 class Course(models.Model):
+    '''
+    This class create model of Course
+    '''
     _name = 'openacademy.course'
 
     name = fields.Char(string='Title', required=True)
