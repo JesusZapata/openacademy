530b383dbddaae5055314feb1c79ded4b39fbf5f
Jesus Antonio Zapata Cruz <chuchocorleone@gmail.com>
2016-12-12 23:00:53 -0400

    [ADD] openacademy: Add folder model and file course.py. Defined class Course model

---
+++ model/course.py
@@ -0,0 +1,9 @@
+from openerp import fields, models
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+
