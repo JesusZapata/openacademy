e96d38f6ad04a9525fe5c72840c6cf9d5322daf7
JesusZapata <jesus@vauxoo.com>
2016-12-14 11:56:32 -0400

    [ADD] openacademy: Adding model and view of partner

---
+++ model/partner.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models
+
+'''
+This module create model of Partner
+'''
+
+
+class Partner(models.Model):
+    """"
+    This class create model of Partner
+    """
+    _inherit = 'res.partner'
+    instructor = fields.Boolean("Instructor", default=False)
+
+    session_ids = fields.Many2many('openacademy.session',
+                                   string="Session as instructor", readonly=True)
