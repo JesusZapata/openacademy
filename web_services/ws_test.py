759db7b0d756b7f86e3706f355531cc1229118d4
JesusZapata <jesus@vauxoo.com>
2016-12-16 15:14:45 -0400

    [ADD] web_services: Find Course 0 and adding new session whit web service

---
+++ web_services/ws_test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import functools
+import xmlrpclib
+
+HOST = 'localhost'
+PORT = 8069
+DB = 'odoo'
+USER = 'admin'
+PASS = 'admin'
+ROOT = 'http://%s:%d/xmlrpc/' % (HOST, PORT)
+
+# 1. Login
+uid = xmlrpclib.ServerProxy(ROOT + 'common').login(DB, USER, PASS)
+print "Logged in as %s (uid:%d)" % (USER, uid)
+
+call = functools.partial(
+    xmlrpclib.ServerProxy(ROOT + 'object').execute,
+    DB, uid, PASS)
+
+sessions = call('openacademy.session', 'search_read', [], ['name', 'seats'])
+for session in sessions:
+    print "Session %s (%s seats)" % (session['name'], session['seats'])
+
+course_id = call('openacademy.course', 'search', [('name', 'ilike', 'Course 0')])[0]
+session_id = call('openacademy.session', 'create', {
+    'name': 'My session',
+    'course_id': course_id,
+})
