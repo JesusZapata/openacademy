dcca78680717e50c90647f56413aa29e1a22956a
Jesus Antonio Zapata Cruz <chuchocorleone@gmail.com>
2016-12-19 05:55:38 -0400

    [ADD] openacademy: Adding test for the model Session

---
+++ openacademy/tests/test_global_openacademy_session.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from openerp.tests.common import TransactionCase
+from openerp.exceptions import ValidationError
+
+
+class GlobalTestOpenAcademySession(TransactionCase):
+    '''
+    Test Model Session
+    '''
+
+    def setUp(self):
+        super(GlobalTestOpenAcademySession, self).setUp()
+        self.session = self.env['openacademy.session']
+        self.partner_vauxoo = self.env.ref('base.res_partner_23')
+        self.course = self.env.ref('openacademy.course1')
+        self.partner_attende = self.env.ref('base.res_partner_5')
+
+    def test_10_instructor_is_attende(self):
+        '''
+        Check _check_instructor_not_in_attendees constrains
+        '''
+        with self.assertRaisesRegexp(
+                ValidationError,
+                "A session's instructor can't be an attendee"
+        ):
+            self.session.create({
+                'name': 'Session test 1',
+                'seats': 1,
+                'instructor_id': self.partner_vauxoo.id,
+                'attendee_ids': [(6, 0, [self.partner_vauxoo.id])],
+                'course_id': self.course.id,
+            })
+
+    def test_20_wkf_done(self):
+        '''
+        Check the workflow
+        '''
+        session_test = self.session.create({
+            'name': 'Session test 1',
+            'seats': 2,
+            'instructor_id': self.partner_vauxoo.id,
+            'attendee_ids': [(6, 0, [self.partner_attende.id])],
+            'course_id': self.course.id,
+        })
+
+        # Check draft state
+        self.assertEqual(session_test.state, 'draft', 'Initial state should be in "draft"')
+
+        # Change confirmed state
+        #session_test.signal_workflow('button_confirm')
+        #self.assertEqual(session_test.state, 'confirmed', "Signal confirm don't work fine!")
+
+        # Change done state
+        #session_test.signal_workflow('button_done')
+        #self.assertEqual(session_test.state, 'done', "Signal done don't work fine!")
